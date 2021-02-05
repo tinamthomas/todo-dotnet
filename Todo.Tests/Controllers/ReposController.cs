d9814d0fa9b9a249fc36b63a12c802cbeabc0f9a
Tina Maria Thomas <tinainink@gmail.com>
2021-02-05 14:24:41 -0500

    Tina | Fix tests

--- Todo.Tests/Controllers/ReposController.cs
+++ Todo.Tests/Controllers/ReposController.cs
@@ -1,14 +1,13 @@
-using System;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using Moq;
 using Moq.Protected;
 using NUnit.Framework;
 using TodoApi.Controllers;
+using TodoApi.Models;
 
 namespace Todo.Tests
 {
@@ -24,24 +23,26 @@ namespace Todo.Tests
         public async Task Test1()
         {
             var httpClientFactory = new Mock<IHttpClientFactory>();  
-            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();  
-            string json = "[{\"name\":\"Person1\"}]";
-
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            Repo repo1 = new Repo("Repo1");
+            Repo repo2 = new Repo("Repo2");
+            Repo[] repos = new Repo[] {repo1, repo2};
+            string reposJson = JsonSerializer.Serialize(repos);
             mockHttpMessageHandler.Protected()  
                 .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())  
                 .ReturnsAsync(new HttpResponseMessage  
                 {  
                     StatusCode = HttpStatusCode.OK,  
-                    Content = new StringContent(json),
+                    Content = new StringContent(reposJson),
                 });  
   
             var client = new HttpClient(mockHttpMessageHandler.Object);
-            httpClientFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);  
-            
+            httpClientFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(client);
             var controller = new ReposController(httpClientFactory.Object);
 
             var result = await controller.GetRepos();
-            Assert.NotNull(result);
+            
+            Assert.AreEqual(reposJson, JsonSerializer.Serialize(result.Value));
         }
     }
 }
--- Todo/Models/Repo.cs
+++ Todo/Models/Repo.cs
@@ -4,6 +4,10 @@ namespace TodoApi.Models
 {
     public class Repo
     {
+        public Repo(string name)
+        {
+            Name = name;
+        }
         [JsonPropertyName("name")] 
         public string Name { get; set; }
     }
